7467404737062b99a7ee4ce74132ee7fe23d2bf5
Sunimt <dorysunim@gmail.com>
2024-05-23 08:49:19 -0700

    primer avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SobremiComponent } from './sobremi/sobremi.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, SobremiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portafolioweb';
+
+  ngOnInit() {
+    if (typeof document !== 'undefined') {
+      initFlowbite();
+    }
+  }
+
 }
